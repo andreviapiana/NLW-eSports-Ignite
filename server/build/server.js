06f0ab577a9151b4b1e0aadc6080e5b3007782bd
André Viapiana <andreviapiana@gmail.com>
2022-09-20 19:23:33 -0300

    :rocket::rocket: DEPLOY :rocket::rocket:

---
+++ server/build/server.js
@@ -0,0 +1,11 @@
+import express from 'express';
+const app = express();
+app.get("/ads", (request, response) => {
+    return response.json([
+        { id: 1, name: "Anuncio 1" },
+        { id: 2, name: "Anuncio 2" },
+        { id: 3, name: "Anuncio 3" },
+    ]);
+});
+const PORT = 3333;
+app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`));
